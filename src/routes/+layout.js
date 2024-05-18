77fd92230519aaf51fe4b525e87282272f8cf223
ocknamo <s14pes@gmail.com>
2024-05-18 23:56:04 +0900

    style: update css

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,3 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
+import './styles.css';
 export const prerender = true;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,17 @@
 
 <h1>SSG nostr Event PoC</h1>
 <hr />
-<SvelteMarkdown source={content}></SvelteMarkdown>
+<div class="main-content">
+	<SvelteMarkdown source={content}></SvelteMarkdown>
+</div>
 <hr />
 <p>
 	Visit <a href="https://github.com/ocknamo/poc-nostr-ssg">poc-nostr-ssg</a> to read the documentation
 </p>
+
+<style>
+	.main-content {
+		margin-left: 100px;
+		max-width: 800px;
+	}
+</style>
